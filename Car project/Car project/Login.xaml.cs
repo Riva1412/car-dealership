aa266169b3cc5a3e365e4d89a4799075cf7ec2ad
BeshoyHani <be4oy.hani@gmail.com>
2019-11-23 14:08:01 +0200

    DataBase was created and connected to Login&signup

--- Car project/Car project/Login.xaml.cs
+++ Car project/Car project/Login.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,22 +11,30 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Text.RegularExpressions; // for regular expression in email
 
-namespace Car_project
+
+namespace Login
 {
     /// <summary>
-    /// Interaction logic for Login.xaml
+    /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class Login : Window
+    public partial class MainWindow : Window
     {
-        public Login()
+        SqlConnection sqlcon = new SqlConnection(@"Data Source=(local);Initial Catalog=Cars_db;Integrated Security=SSPI");
+
+
+        public MainWindow()
         {
             InitializeComponent();
+            loginMail.Focus();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void loginShowBtn_Click(object sender, RoutedEventArgs e)
         {
+            loginMail.Focus();
             loginShowBtn.Foreground = Brushes.Black;
             loginShowBtn.Background = Brushes.White;
             signUpShowBtn.Background = (Brush)new BrushConverter().ConvertFrom("#FF9C27B0");
@@ -34,14 +43,160 @@ namespace Car_project
             signUpPanel.Visibility = Visibility.Hidden;
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        private void signUpShowBtn_Click(object sender, RoutedEventArgs e)
         {
+            emailSignUpTxt.Focus();
             signUpShowBtn.Foreground = Brushes.Black;
             signUpShowBtn.Background = Brushes.White;
             loginShowBtn.Background = (Brush)new BrushConverter().ConvertFrom("#FF9C27B0");
             loginShowBtn.Foreground = Brushes.White;
             loginPanel.Visibility = Visibility.Hidden;
             signUpPanel.Visibility = Visibility.Visible;
+        }
+
+
+        private int check_Login(string email, string password)
+        {
+            try
+            {
+                if (sqlcon.State == System.Data.ConnectionState.Closed)
+                    sqlcon.Open();
+
+                string query = "select count(1) from UserData where Email=@usermail and UserPassword=@password";
+                SqlCommand sqlcmd = new SqlCommand(query, sqlcon);
+                sqlcmd.Parameters.AddWithValue("@usermail", email);
+                sqlcmd.Parameters.AddWithValue("@password", password);
+                return Convert.ToInt32(sqlcmd.ExecuteScalar());
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            return 0;
+
+        }
+
+        private void signInBtn_Function()
+        {
+            int count = check_Login(loginMail.Text, loginPassword.Password);
+            if (count == 1)
+            {
+                string query = "select FirstName from UserData where Email=@usermail and UserPassword=@password";
+                SqlCommand sqlcmd = new SqlCommand(query, sqlcon);
+
+                sqlcmd = new SqlCommand(query, sqlcon);
+                sqlcmd.Parameters.AddWithValue("@usermail", loginMail.Text);
+                sqlcmd.Parameters.AddWithValue("@password", loginPassword.Password);
+                string userName = Convert.ToString(sqlcmd.ExecuteScalar());
+                MessageBox.Show("Welcome " + userName);
+            }
+            else MessageBox.Show("Wrong username or password");
+
+            sqlcon.Close();
+        }
+
+
+
+        private void signUpBtn_Function()
+        {
+            if (emailSignUpTxt.Text == "" || firstNameTxt.Text == "" || secondNameTxt.Text == "" || adressTxt.Text == "" || phoneTxt.Text == "" || passwordSignUpTxt.Password == "" || confPasswordSignUpTxt.Password == "")
+            {
+                MessageBox.Show("You must fill in all fields");
+                return;
+            }
+
+            if (passwordSignUpTxt.Password != confPasswordSignUpTxt.Password)
+            {
+                MessageBox.Show("Password does not match");
+                return;
+            }
+
+
+            if (phoneTxt.Text.Length != 11)
+            {
+                MessageBox.Show("Invalid phone number");
+                return;
+            }
+
+            // Regular expression
+            Regex regex = new Regex(@"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$");
+            bool isValid = regex.IsMatch(emailSignUpTxt.Text.Trim());
+            if (!isValid)
+            {
+                System.Windows.MessageBox.Show("Invalid Email.");
+                return;
+            }
+            //------------------------------//
+
+
+            int count = check_Login(emailSignUpTxt.Text, passwordSignUpTxt.Password);
+            if (count == 1)
+                MessageBox.Show("This account is already registered");
+
+            else
+            {
+                string query = "insert into userdata(Email, UserPassword,  FirstName, SecondName, Phone, Address)" +
+                    "values(@email, @password,  @FirstName, @SecondName, @Phone, @Address)";
+
+                SqlCommand sqlcmd = new SqlCommand(query, sqlcon);
+                sqlcmd.Parameters.AddWithValue("@email", emailSignUpTxt.Text);
+                sqlcmd.Parameters.AddWithValue("@password", passwordSignUpTxt.Password);
+                sqlcmd.Parameters.AddWithValue("@FirstName", firstNameTxt.Text);
+                sqlcmd.Parameters.AddWithValue("@SecondName", secondNameTxt.Text);
+                sqlcmd.Parameters.AddWithValue("@Phone", phoneTxt.Text);
+                sqlcmd.Parameters.AddWithValue("@Address", adressTxt.Text);
+
+                sqlcmd.ExecuteNonQuery();
+            }
+
+            MessageBox.Show("You have registered successfully");
+            sqlcon.Close();
+        }
+
+
+        private void signInBtn_Click(object sender, RoutedEventArgs e)
+        {
+            signInBtn_Function();
+        }
+
+        private void signInBtn_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                signInBtn_Function();
+            }
+        }
+
+        private void loginPassword_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                signInBtn_Function();
+            }
+            
+        }
+
+
+
+
+        private void SignUpBtn_Click(object sender, RoutedEventArgs e)
+        {
+            signUpBtn_Function();
+
+        }
+        private void signUpBtn_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                signUpBtn_Function();
+            }
+        }
+
+        private void cancelBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
