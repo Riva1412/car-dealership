d9df709cf7595faec5067386ce806a677165336c
IsaacMo12 <48593866+IsaacMo12@users.noreply.github.com>
2019-12-11 20:35:17 +0200

    UserControls2

--- Car project/Car project/User.xaml.cs
+++ Car project/Car project/User.xaml.cs
@@ -18,156 +18,27 @@ using System.Windows.Shapes;
 
 namespace Car_project
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class User 
+    public partial class User : Window
     {
+        UserProfile userprofile_obj = new UserProfile();
+        AllProducts AllProducts_obj = new AllProducts();
         public User()
         {
             InitializeComponent();
-        }
-        private void shownamedate()
-        {
-            FirstNameText.Text =  DBManager.getUserData("FirstName", GlobalVars.userid);
-            SecondNameText.Text = DBManager.getUserData("SecondName", GlobalVars.userid);
-
-        }
-        private void showinfodate()
-        {
-            PhoneText.Text = DBManager.getUserData("Phone", GlobalVars.userid);
-            EmailText.Text = DBManager.getUserData("Email", GlobalVars.userid);
-            AddressText.Text = DBManager.getUserData("Address", GlobalVars.userid);
+            // add profile object
+            UserWindow.Children.Insert(1,userprofile_obj);
+            userprofile_obj.Visibility = Visibility.Hidden;
+            // add all products object
+            UserWindow.Children.Insert(1,AllProducts_obj);
+            AllProducts_obj.Visibility = Visibility.Hidden;
         }
 
-        private void ButtonCloseMenu_Click(object sender, RoutedEventArgs e)
-        {
-            ButtonCloseMenu.Visibility = Visibility.Collapsed;
-            ButtonOpenMenu.Visibility = Visibility.Visible;
-
-        }
-
-        private void ButtonOpenMenu_Click(object sender, RoutedEventArgs e)
-        {
-            ButtonCloseMenu.Visibility = Visibility.Visible;
-            ButtonOpenMenu.Visibility = Visibility.Collapsed;
-        }
-
-        private void Home(object sender, RoutedEventArgs e)
-        {
-            ShowProducts.Visibility = Visibility.Hidden;
-            MessageBox.Show("Home");
-
-        }
-
-        /// profile begin
-        private void ProfileBtn_Click(object sender, RoutedEventArgs e)
-        {
-            UserProfile x = new UserProfile();
-            UserWindow.Children.Add(x);
-            x.Visibility = Visibility.Hidden;
-            ShowProducts.Visibility = Visibility.Hidden;
-            Profile.Visibility = Visibility.Visible;
-            try
-            {
-                shownamedate();
-                showinfodate();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-      
-        }
-        // name expander
-        private void Nameconfirm_click(object sender, RoutedEventArgs e)
-        {
-            DBManager.updateUserData("FirstName", FirstNameText.Text, GlobalVars.userid);
-            DBManager.updateUserData("SecondName", SecondNameText.Text, GlobalVars.userid);
-            Nameexpander.IsExpanded = false;
-        }
-        private void Namecancel_click(object sender, RoutedEventArgs e)
-        {
-            Nameexpander.IsExpanded = false;
-            shownamedate();
-        }
-        //-------------------------------
-        // pass expander
-        private void passconfirm_click(object sender, RoutedEventArgs e)
-        {
-            if (Newpass.Password.Length > 0 && Newpass.Password != ConfPass.Password)
-            {
-                MessageBox.Show("Password does not match");
-                return;
-            }
-            if (Newpass.Password.Length==0)
-            {
-                MessageBox.Show("type at least one character");
-                return;
-            }
-            DBManager.updateUserData("UserPassword", Newpass.Password, GlobalVars.userid);
-            Newpass.Password = ConfPass.Password= "";
-            passwordexpander.IsExpanded = false;
-        }
-        private void passcancel_click(object sender, RoutedEventArgs e)
-        {
-            passwordexpander.IsExpanded = false;
-            Newpass.Password = ConfPass.Password = "";
-        }
-        //-------------------------------------------------------------
-        // info expander
-        private void infoconfrim_click(object sender, RoutedEventArgs e)
-        {
-            if (PhoneText.Text.Length != 11)
-            {
-                MessageBox.Show("Invalid phone number");
-                return;
-            }
-            DBManager.updateUserData("Email", EmailText.Text, GlobalVars.userid);
-            DBManager.updateUserData("Phone", PhoneText.Text, GlobalVars.userid);
-            DBManager.updateUserData("Address", AddressText.Text, GlobalVars.userid);
-            infoexpander.IsExpanded = false;
-        }
-
-        private void infocancel_click(object sender, RoutedEventArgs e)
-        {
-            infoexpander.IsExpanded = false;
-            showinfodate();
-        }
-        //----------------------------
-        // profile end
-        private void Products(object sender, RoutedEventArgs e)
-        {
-            Profile.Visibility = Visibility.Hidden;
-            ShowProducts.Visibility = Visibility.Visible;
-
-            
-            var CarProducts = DBManager.getAllProducts();
-            if (CarProducts.Count > 0)
-                ListViewProducts.ItemsSource = CarProducts;
-            else
-                MessageBox.Show("No Avaiable Products");
-        }
-        private void Cart(object sender, RoutedEventArgs e)
-        {
-            ShowProducts.Visibility = Visibility.Hidden;
-            MessageBox.Show("Cart");
-        }
-
-
-        /// logout
-        private void Logout(object sender, RoutedEventArgs e)
-        {
-            Login.MainWindow loginwinow = new Login.MainWindow();
-            loginwinow.Show();
-            this.Close();
-        }
-
-        /// close maxmisze minimize
+        // left bar 
         private void Buttonclose_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
         }
+
         private void ButtonMaximize_Click(object sender, RoutedEventArgs e)
         {
             if (this.WindowState == System.Windows.WindowState.Normal)
@@ -184,10 +55,69 @@ namespace Car_project
         {
             this.WindowState = System.Windows.WindowState.Minimized;
         }
-        private void buy_click(object sender, RoutedEventArgs e)
+
+        // close and open menu
+
+        private void ButtonCloseMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonCloseMenu.Visibility = Visibility.Collapsed;
+            ButtonOpenMenu.Visibility = Visibility.Visible;
+
+
+        }
+        private void ButtonOpenMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonCloseMenu.Visibility = Visibility.Visible;
+            ButtonOpenMenu.Visibility = Visibility.Collapsed;
+        }
+
+        // Home
+
+        private void Home(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Home");
+        }
+
+        // Profile
+
+        private void ProfileBtn_Click(object sender, RoutedEventArgs e)
+        {
+            AllProducts_obj.Visibility = Visibility.Hidden;
+
+            userprofile_obj.Visibility = Visibility.Visible;
+        }
+
+       // Cars
+
+        private void Cars_btn_click(object sender, RoutedEventArgs e)
+        {
+            userprofile_obj.Visibility = Visibility.Hidden;
+
+            AllProducts_obj.Visibility = Visibility.Visible;
+        }
+        
+        // Car Parts
+        private void CarsParts_btn_click(object sender, RoutedEventArgs e)
         {
 
         }
+        // Cart
+
+        private void Cart_click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Cart");
+        }
+
+        // logout
+
+        private void Logout_clcik(object sender, RoutedEventArgs e)
+        {
+            Login.MainWindow loginwinow = new Login.MainWindow();
+            this.Close();
+            loginwinow.Show();
+        }
+
+
         SqlConnection sqlcon = new SqlConnection(@"Data Source=(local);Initial Catalog=Cars_db;Integrated Security=SSPI");
 
         private void Add_Product(object sender, RoutedEventArgs e)
@@ -252,7 +182,7 @@ namespace Car_project
 
             return products;
         }
-        private void My_Products(object sender, RoutedEventArgs e)
+      /*  private void My_Products(object sender, RoutedEventArgs e)
         {
             ProductsGrid.Visibility = Visibility.Hidden;
             var CarProducts = GetMyProducts();
@@ -260,22 +190,11 @@ namespace Car_project
                 ListViewProducts.ItemsSource = CarProducts;
             ShowProducts.Visibility = Visibility.Visible;
 
-        }
+        }*/
 
         private void Add_Products(object sender, RoutedEventArgs e)
         {
-            ProductsGrid.Visibility = Visibility.Hidden;
             AddProduct.Visibility = Visibility.Visible;
-
-        }
-
-        private void All_Products(object sender, RoutedEventArgs e)
-        {
-            ProductsGrid.Visibility = Visibility.Hidden;
-            var CarProducts =DBManager.getAllProducts();
-            if (CarProducts.Count > 0)
-                ListViewProducts.ItemsSource = CarProducts;
-            ShowProducts.Visibility = Visibility.Visible;
 
         }
     }
--- Car project/Car project/UserWindow_Classes/UserProfile.xaml.cs
+++ Car project/Car project/UserWindow_Classes/UserProfile.xaml.cs
@@ -23,6 +23,77 @@ namespace Car_project
         public UserProfile()
         {
             InitializeComponent();
+            shownamedate();
+            showinfodate();
         }
+        private void shownamedate()
+        {
+            FirstNameText.Text = DBManager.getUserData("FirstName", GlobalVars.userid);
+            SecondNameText.Text = DBManager.getUserData("SecondName", GlobalVars.userid);
+
+        }
+        private void showinfodate()
+        {
+            PhoneText.Text = DBManager.getUserData("Phone", GlobalVars.userid);
+            EmailText.Text = DBManager.getUserData("Email", GlobalVars.userid);
+            AddressText.Text = DBManager.getUserData("Address", GlobalVars.userid);
+        }
+        // name expander
+        private void Nameconfirm_click(object sender, RoutedEventArgs e)
+        {
+            DBManager.updateUserData("FirstName", FirstNameText.Text, GlobalVars.userid);
+            DBManager.updateUserData("SecondName", SecondNameText.Text, GlobalVars.userid);
+            Nameexpander.IsExpanded = false;
+        }
+        private void Namecancel_click(object sender, RoutedEventArgs e)
+        {
+            Nameexpander.IsExpanded = false;
+            shownamedate();
+        }
+        //-------------------------------
+        // pass expander
+        private void passconfirm_click(object sender, RoutedEventArgs e)
+        {
+            if (Newpass.Password.Length > 0 && Newpass.Password != ConfPass.Password)
+            {
+                MessageBox.Show("Password does not match");
+                return;
+            }
+            if (Newpass.Password.Length == 0)
+            {
+                MessageBox.Show("type at least one character");
+                return;
+            }
+            DBManager.updateUserData("UserPassword", Newpass.Password, GlobalVars.userid);
+            Newpass.Password = ConfPass.Password = "";
+            passwordexpander.IsExpanded = false;
+        }
+        private void passcancel_click(object sender, RoutedEventArgs e)
+        {
+            passwordexpander.IsExpanded = false;
+            Newpass.Password = ConfPass.Password = "";
+        }
+        //-------------------------------------------------------------
+        // info expander
+        private void infoconfrim_click(object sender, RoutedEventArgs e)
+        {
+            if (PhoneText.Text.Length != 11)
+            {
+                MessageBox.Show("Invalid phone number");
+                return;
+            }
+            DBManager.updateUserData("Email", EmailText.Text, GlobalVars.userid);
+            DBManager.updateUserData("Phone", PhoneText.Text, GlobalVars.userid);
+            DBManager.updateUserData("Address", AddressText.Text, GlobalVars.userid);
+            infoexpander.IsExpanded = false;
+        }
+
+        private void infocancel_click(object sender, RoutedEventArgs e)
+        {
+            infoexpander.IsExpanded = false;
+            showinfodate();
+        }
+        //----------------------------
+        // profile end
     }
 }
